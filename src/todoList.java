f0b4d0fa11aa32dd4966f033a153520a044f79fb
Keegikuskil <jannupoiss007@gmail.com>
2026-03-27 09:42:29 +0200

    lisaülesanne

--- src/todoList.java
+++ src/todoList.java
@@ -1,3 +1,10 @@
-public class todoList {
+import java.util.ArrayList;
+import java.util.List;
 
+public class todoList {
+    private List<Ülesanne> ülesanded = new ArrayList<>();
+
+    public void lisaÜlesanne(Ülesanne ülesanne) {
+        ülesanded.add(ülesanne);
+    }
 }
