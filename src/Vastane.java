7d5a6323e8c62725ec1f1f732aa3f34a91224595
Keegikuskil <jannupoiss007@gmail.com>
2026-03-30 08:12:08 +0300

    getpunktid

--- src/Vastane.java
+++ src/Vastane.java
@@ -1,13 +1,16 @@
 import java.util.List;
 
-public class Vastane {
-    void eludeArv(Kasutaja kasutaja) {
+public abstract class Vastane {
+    public void eludeArv(Kasutaja kasutaja) {
         List<Ülesanne> ülesanded = kasutaja.getÜlesanded();
+
         for (int i=0, elusid=0; i<ülesanded.size(); i++) {
             //elusid += prioriteedid().get(ülesanded(i));
         //võtta ülesanne ning tagastada selle prioriteedi punktiarv prioriteetPunktideks abiga
         }
 
     };
-    // void saaHaiget();
+    public abstract void saaHaiget();
+
+    public abstract void teeHaiget();
 }
